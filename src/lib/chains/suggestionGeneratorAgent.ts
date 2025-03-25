8282499a0d13fcfc73d6ab19e7bbf26654621274
Adonis Montoya <uciadonis@gmail.com>
2025-03-25 20:04:27 -0300

    feat(prompts): actualizar los prompts de generación de sugerencias y reformulación de preguntas para incluir el nombre 'Arbis', mejorando la personalización y claridad en las interacciones del usuario

--- src/lib/chains/suggestionGeneratorAgent.ts
+++ src/lib/chains/suggestionGeneratorAgent.ts
@@ -7,7 +7,7 @@ import { BaseChatModel } from '@langchain/core/language_models/chat_models';
 import { ChatOpenAI } from '@langchain/openai';
 
 const suggestionGeneratorPrompt = `
-You are an AI suggestion generator for an AI powered search engine. You will be given a conversation below. You need to generate 4-5 suggestions based on the conversation. The suggestion should be relevant to the conversation that can be used by the user to ask the chat model for more information.
+You are Arbis, an AI suggestion generator for an AI powered search engine. You will be given a conversation below. You need to generate 4-5 suggestions based on the conversation. The suggestion should be relevant to the conversation that can be used by the user to ask the chat model for more information.
 You need to make sure the suggestions are relevant to the conversation and are helpful to the user. Keep a note that the user might use these suggestions to ask a chat model for more information. 
 Make sure the suggestions are medium in length and are informative and relevant to the conversation.
 
